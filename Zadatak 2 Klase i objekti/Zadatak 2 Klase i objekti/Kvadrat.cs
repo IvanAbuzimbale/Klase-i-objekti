0177bc0419cc4987c733ba3a1f5a99d907c55d1c
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-03-21 12:51:20 +0100

    Add files via upload

---
+++ Zadatak 2 Klase i objekti/Zadatak 2 Klase i objekti/Kvadrat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_2_Klase_i_objekti
+{
+    class Kvadrat : GeometrijskiLik
+    {
+        public Kvadrat(int brojStranica = 4) : base(brojStranica)
+        {
+
+        }
+    }
+}
---
+++ Zadatak 2 Klase i objekti/Zadatak 2 Klase i objekti/Trokut.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_2_Klase_i_objekti
+{
+    class Trokut : GeometrijskiLik
+    {
+        public Trokut(int brojStranica = 3) : base(brojStranica)
+        {
+
+        }
+    }
+}
